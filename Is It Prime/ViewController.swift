2de3ef09a00a0bb11cbf8544e171ce25f9719f06
Alexander Abushady <aabushady214@gmail.com>
2019-07-23 13:14:03 -0400

    Cleaned Up
    Cleaned up code fixed error where 0 was considered a prime number.  Added comments throughout. Version 1 is finished.

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -15,25 +15,44 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // This will look for a tap within the View Controller. If it finds a tap it will use the dimissKeyBoard function.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:Selector(("dismissKeyBoard")))
+        view.addGestureRecognizer(tap)
+    }
+    
+    // Add Objective-C function that dismisses the software keyboard.
+    @objc func dismissKeyBoard() {
+        view.endEditing(true)
     }
     
     @IBAction func calculateTapped(_ sender: Any) {
+        // The i variable is used to regulate the while loop and check if number is prime or not.
         var i = 2
+        // This bool tells whether a number is prime or not.
         var isPrime = true
         
+        // If statement is for making sure that nil wont be passed.
         if Int(inputField.text!) != nil {
+            // Pass input into the constant number.
             let number = Int(inputField.text!)
-            if number == 1 {
+            
+            // 0 and 1 can never be a prime number!
+            if number! <= 1 {
                 isPrime = false
             }
             
+            // While loop that checks with i if a number can be divisible by any number besides 1 and itself.
             while i < number! {
                 if number! % i == 0 {
                     isPrime = false
+                    // If a number cleanly divides we can stop the loop.
                     break
                 }
+                // Iterating the loop.
                 i += 1
             }
+            // When checking is complete we can give the results!
             requestLabel.text = "The Result Is..."
             requestLabel.textColor = UIColor.black
             if isPrime {
@@ -43,11 +62,13 @@ class ViewController: UIViewController {
                 primeLabel.text = "Not Prime..."
                 primeLabel.textColor = UIColor.red
             }
+        // If invalid input is entered the user is told so.
         } else {
             requestLabel.text = "Positive whole number..."
             requestLabel.textColor = UIColor.red
             primeLabel.text = ""
         }
+        // Dismiss the keyboard when the button is pressed.
         self.view.endEditing(true)
     }
 
